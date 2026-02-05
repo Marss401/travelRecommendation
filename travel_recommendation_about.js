23c04c1b4e36496bd2e03388bda5e68a9c4e9165
Marss401 <salamiabidemi19@gmail.com>
2026-02-05 12:08:16 -0500

    message

--- travel_recommendation_about.js
+++ travel_recommendation_about.js
@@ -22,12 +22,14 @@ const members = [
 const teamMember = document.querySelector(".about_members");
 members.forEach((member) => {
     teamMember.innerHTML += `
-    <div class="about_icon">
-    <i class="fa-solid fa-user"></i>
-  </div>
-  <div class="about_details">
-    <h3 class="user_name">${member.name}</h3>
-    <p class="user_description">${member.description}</p>
-    <button type="button" class="user_role">${member.role}</button>
-  </div>`
+    <div class="about_member">
+      <div class="about_icon">
+        <i class="fa-solid fa-user"></i>
+      </div>
+      <div class="about_details">
+        <h3 class="user_name">${member.name}</h3>
+        <p class="user_description">${member.description}</p>
+        <button type="button" class="user_role">${member.role}</button>
+      </div>
+    </div>`
 })
