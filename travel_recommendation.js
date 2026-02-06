4f97d18086196b51b7b8a07f4ddf44d6353928ac
Marss401 <salamiabidemi19@gmail.com>
2026-02-06 18:39:34 -0500

    message

--- travel_recommendation.js
+++ travel_recommendation.js
@@ -11,16 +11,35 @@ function searchResult(event){
     fetch('travel_recommendation_api.json')
     .then(response => response.json())
     .then(data => {
-         console.log("FULL DATA:", data);
-         console.log("COUNTRIES:", data.countries);
-         console.log("FIRST COUNTRY:", data.countries[0]);
-        const country = data.countries.find(item => item.name.toLowerCase() === searchInput);
+        let results = [];
 
-      if (country) {
-        console.log("MATCH FOUND:", country);
-      } else {
-        console.log("NO MATCH");
-      }
+        if(searchInput === "beach"){
+            results = data.beaches;
+        } else if(searchInput === "country"){
+            results = data.countries;
+        }else if (searchInput === "temple"){
+            results = data.temples;
+        }
+
+        if(searchInput === 0){
+            resultDiv.innerHTML += `<p>No Results found!</p>`
+            return;
+        }
+        results.forEach(result => {
+            console.log(result);
+            resultDiv.innerHTML += `
+                <div class="result_card">
+                    <img class="result_img" src="${result.imageUrl}" alt="${result.name}">
+                    <div class="result_content">
+                        <h3 class="result_name">${result.name}</h3>
+                        <p class="result_description">${result.description}</p>
+                        <button type="button" class="result_button">Visit</button>
+                    </div>
+                </div>
+            `
+
+        });
+        console.log("Results:", results);
         
     })
         .catch(error => {
